6a113042a2750c601b7793497e83521a7a410d03
Taylor Moore <Tmoore2272@gmail.com>
2016-06-06 19:17:15 -0600

    Adds start to search, still some bugs

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,15 @@
+function searchLinks() {
+  $('#search').keyup(function() {
+    var context = $(this).val().toLowerCase();
+    var $links = $('.link');
+    $links.each(function(index, link) {
+      $link = $(link)
+      text = $link.find('.content').text().toLowerCase();
+      if (text.indexOf(context) >= 0) {
+        $link.show()
+      } else {
+        $link.hide()
+      }
+    })
+  })
+}
